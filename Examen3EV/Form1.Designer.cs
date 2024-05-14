8b76e47dc1e4041d411359978eebc259e3cc4a75
jrgs <jrgarcia@iesmarenostrum.com>
2024-05-14 18:33:50 +0200

    Proyecto para 2ª Evaluación: Normas de estilo, documentación, refactorización

---
+++ Examen3EV/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Examen3EV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDiccionario = new System.Windows.Forms.TextBox();
+            this.txtFrase1 = new System.Windows.Forms.TextBox();
+            this.txtFrase2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDiccionario
+            // 
+            this.txtDiccionario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiccionario.Location = new System.Drawing.Point(458, 49);
+            this.txtDiccionario.Multiline = true;
+            this.txtDiccionario.Name = "txtDiccionario";
+            this.txtDiccionario.Size = new System.Drawing.Size(321, 385);
+            this.txtDiccionario.TabIndex = 0;
+            // 
+            // txtFrase1
+            // 
+            this.txtFrase1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFrase1.Location = new System.Drawing.Point(30, 49);
+            this.txtFrase1.Multiline = true;
+            this.txtFrase1.Name = "txtFrase1";
+            this.txtFrase1.Size = new System.Drawing.Size(411, 112);
+            this.txtFrase1.TabIndex = 1;
+            // 
+            // txtFrase2
+            // 
+            this.txtFrase2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFrase2.Location = new System.Drawing.Point(30, 202);
+            this.txtFrase2.Multiline = true;
+            this.txtFrase2.Name = "txtFrase2";
+            this.txtFrase2.Size = new System.Drawing.Size(411, 113);
+            this.txtFrase2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(453, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Diccionario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Frase 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Frase 2";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(39, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(402, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Analizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFrase2);
+            this.Controls.Add(this.txtFrase1);
+            this.Controls.Add(this.txtDiccionario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDiccionario;
+        private System.Windows.Forms.TextBox txtFrase1;
+        private System.Windows.Forms.TextBox txtFrase2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Examen3EV/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen3EV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Diccionario diccionario= new Diccionario();
+            int resultado = diccionario.analizar(txtFrase1.Text, txtFrase2.Text);
+            if (resultado == 0)
+            {
+                for (int i = 0; i < diccionario.words.Count; i++)
+                    txtDiccionario.Text += diccionario.words[i] + Environment.NewLine;
+            }
+            else if (resultado == -1)
+                MessageBox.Show("La frase 1 no es correcta");
+            else if (resultado == -2)
+                MessageBox.Show("La frase 2 no es correcta");
+        }
+    }
+}
